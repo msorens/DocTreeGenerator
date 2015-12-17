7e228d58f44e4f5792968e12b69938e3695b3259
msorens <msorens@users.noreply.github.com>
2015-12-17 14:09:58 -0800

    Corrected template path; removed old licensing info; added copyright.

--- DocTreeGenerator.psm1
+++ DocTreeGenerator.psm1
@@ -1,45 +1,8 @@
-﻿#
-# ==============================================================
-# @ID       $Id: DocTreeGenerator.psm1 1558 2015-11-08 21:55:14Z ms $
-# @created  2011-08-01
-# @project  http://cleancode.sourceforge.net/
-# ==============================================================
+﻿# Copyright (c) 2014-2015 Michael Sorens
+# https://github.com/msorens/DocTreeGenerator
 #
-# The official license for this file is shown next.
-# Unofficially, consider this e-postcardware as well:
-# if you find this module useful, let us know via e-mail, along with
-# where in the world you are and (if applicable) your website address.
-#
-#
-# ***** BEGIN LICENSE BLOCK *****
-# Version: MPL 1.1
-#
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
-#
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is part of the CleanCode toolbox.
-#
-# The Initial Developer of the Original Code is Michael Sorens.
-# Portions created by the Initial Developer are Copyright (C) 2011
-# the Initial Developer. All Rights Reserved.
-#
-# Contributor(s):
-#
-# ***** END LICENSE BLOCK *****
-#
-
-Set-StrictMode -Version Latest
 #requires -version 3.0
-
-[System.Reflection.Assembly]::LoadWithPartialName("System.web") | Out-Null
-
+Set-StrictMode -Version Latest
 
 <#
 
@@ -368,7 +331,7 @@ function Init-Variables()
 
 	$script:namespace_overview_filename = "namespace_overview.html"
 	$script:module_overview_filename    = "module_overview.html"
-	$script:default_template            = "$PSScriptRoot\psdoc_template.html"
+	$script:default_template            = "$PSScriptRoot\Templates\psdoc_template.html"
 
 	# Get the name of *this* module because it requires special handling.
 	if ($script:MyInvocation.MyCommand.Path -match "Modules\\(.*)\\\w*.psm1")
